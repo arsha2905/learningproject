f8a76e3e6125f87ce50896e9f6b24c5df7c85ec8
arsha <arsha@192.168.1.43>
2024-08-06 13:12:01 +0530

    learning example

---
+++ learningrjava/src/learningrpackage/LearningExample.java
@@ -0,0 +1,10 @@
+package learningrpackage;
+
+public class LearningExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("course:java");
+	}
+
+}
